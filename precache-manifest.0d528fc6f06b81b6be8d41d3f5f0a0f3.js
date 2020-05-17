f1e7586baf2fc81f9312a2303fb38a38a9e4ab8c
Nick Shelton <nickshelton1376@gmail.com>
2020-05-17 08:06:49 -0400

    Updates

---
+++ precache-manifest.0d528fc6f06b81b6be8d41d3f5f0a0f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b9fa353c410093b0cc49046368535d",
+    "url": "/complete-react-dev/index.html"
+  },
+  {
+    "revision": "d9870d88a62098a68249",
+    "url": "/complete-react-dev/static/css/main.f72d8a4f.chunk.css"
+  },
+  {
+    "revision": "b5cfb1d9e10fbe1ab627",
+    "url": "/complete-react-dev/static/js/2.1b0bc77a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/complete-react-dev/static/js/2.1b0bc77a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9870d88a62098a68249",
+    "url": "/complete-react-dev/static/js/main.b3241745.chunk.js"
+  },
+  {
+    "revision": "84eb0139f3cd05efa8f3",
+    "url": "/complete-react-dev/static/js/runtime-main.1c0b90af.js"
+  }
+]);
